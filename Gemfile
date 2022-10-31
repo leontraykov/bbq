f41fa24a9bc1696187dae2472dc4f2d410917f5c
leont <leon.traykov@gmail.com>
2022-10-31 13:13:02 -0400

    postgresql only

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem "bootstrap", "~> 5.1.3"
 gem "devise"
 gem "jbuilder"
 gem "importmap-rails"
+gem "pg"
 gem "puma", "~> 5.0"
 gem "rails", "~> 7.0.4"
 gem "sprockets-rails"
@@ -21,11 +22,4 @@ end
 
 group :development do
   gem "web-console"
-end
-
-group :development do
-  gem "sqlite3", "~> 1.4"
-end
-group :production do
-  gem "pg"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_29_002817) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_29_002817) do
     t.datetime "datetime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
