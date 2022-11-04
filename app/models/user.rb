911827321d79d7f2ca192cb89728e95de2f1a85b
leont <leon.traykov@gmail.com>
2022-11-04 19:28:38 -0400

    I18n and login forms edited

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  has_many :events
+  has_many :events, dependent: :delete_all
 
   validates :name, presence: true, length: {maximum: 35}
 
